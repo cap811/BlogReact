96a46496627d75948c1917953731caa3b52bf12f
Nick <xa12850328@student.karazin.ua>
2022-03-27 01:40:54 +0300

    Add creating and deleting blogs with POST and GET from json serv

--- client/src/BlogList.js
+++ client/src/BlogList.js
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 const BlogList = ({bloglist, title}) => {
 
     return(
@@ -6,8 +8,10 @@ const BlogList = ({bloglist, title}) => {
             <h2>{title}</h2>
             {bloglist.map((blog) => (
             <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
+                <Link to={`blogs/${blog.id}`}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </Link>
             </div>
             ))}
         </div>
